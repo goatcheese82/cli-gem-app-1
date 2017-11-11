c3c8352eb34070714703517d59c314850a612f3d
goatcheese82 <nonsquishy@gmail.com>
2017-11-11 11:39:01 -0600

    started moving around some objects to organize app better.

--- lib/daily_meal/cli.rb
+++ lib/daily_meal/cli.rb
@@ -25,8 +25,7 @@ class DailyMeal::CLI
 
             case input
             when "1"
-                @recipes = DailyMeal::Food.food_pick('chicken')
-
+                @recipe = DailyMeal::Food.food_pick('chicken')
             when "2"
                 @recipes = DailyMeal::Food.food_pick('burger')
             when "3"
@@ -51,16 +50,11 @@ class DailyMeal::CLI
             2. Directions
             3. Different Recipe
             4. Return
-            food_options
-
-        food_input = nil
-
-
-            puts "Please make a numerical selection"
-
-            food_input = gets.strip.downcase
+        food_options
+        food_input = gets.strip.downcase
 
                 if food_input == "1"
+                    puts DailyMeal::Food.ingredient_getter
                     puts @ingredients
                 elsif food_input == "2"
                     puts @directions
@@ -71,7 +65,7 @@ class DailyMeal::CLI
                 else puts "Please make a numerical selection"
                 
             end
-            self.food_menu
+            self.selection_menu
     end
 
     def goodbye
--- lib/daily_meal/food.rb
+++ lib/daily_meal/food.rb
@@ -1,41 +1,30 @@
 class DailyMeal::Food
 
-    attr_accessor :name, :ingredients, :directions, :food_cat
+    attr_accessor :title, :ingredients, :directions, :food_cat
 
-    
-    
     def self.food_pick(selection)
-        recipe = self.new
-        @food_cat = "http://www.seriouseats.com/tags/recipes/#{selection}"
+        @food_cat = "http://seriouseats.com/tags/recipes/#{selection}"
         self.link_scraper
         self.ingredient_getter
         self.direction_getter
-        puts @recipe_link     
+        puts @recipe_link
     end
-
-
-        
-
-
 
     def self.link_scraper
-    
-        recipe_url = Nokogiri::HTML(open("#{@food_cat}"))
-    
-        recipe_links = []
-        links = recipe_url.css('.module__link')
-        links.each{|link| recipe_links << link['href']}
-    
-        @recipe_link = recipe_links.sample
-    end
-
-
-    ###Do Not Touch###
+        
+            recipe_url = Nokogiri::HTML(open("#{@food_cat}"))
+        
+            recipe_links = []
+            links = recipe_url.css('.module__link')
+            links.each{|link| recipe_links << link['href']}
+        
+            @recipe_link = recipe_links.sample
+        end
 
     def self.ingredient_getter
         recipe_spec = Nokogiri::HTML(open("#{@recipe_link}"))
 
-        @ingredients = []
+        @@ingredients = []
         ingredient_list = recipe_spec.css('.recipe-ingredients')
         ingredient_list.each{|spec| @ingredients << spec.text.strip}
         
@@ -44,13 +33,13 @@ class DailyMeal::Food
     def self.direction_getter
         recipe_direction = Nokogiri::HTML(open("#{@recipe_link}"))
 
-        @directions = []
+        @@directions = []
         direction_list = recipe_direction.css('.recipe-procedures-list.instructions')
         direction_list.each{|step| @directions << step.text.strip}
     end
 
 
-end
 
-        
+
+end   
         
